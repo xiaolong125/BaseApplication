f77d3a2e29f52060bb22862390b010e78b771692
xiaolong125 <2247081635@qq.com>
2019-05-06 17:59:15 +0800

    添加工具类

---
+++ commonlibrary/src/main/java/com/example/beyond/utils/ListUtil.java
@@ -0,0 +1,21 @@
+package com.example.beyond.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListUtil{
+    //List去重
+    public static <T> List<T> removeDuplicate(List<T> list){
+        List<T> newList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++){
+            if(newList.contains(list.get(i))){
+                //将之前的删掉，添加新的，保持顺序
+                newList.remove(list.get(i));
+                newList.add(list.get(i));
+            }else {
+                newList.add(list.get(i));
+            }
+        }
+        return newList;
+    }
+}
---
+++ commonlibrary/src/main/java/com/example/beyond/utils/httpUtil.java
@@ -0,0 +1,14 @@
+package com.example.beyond.utils;
+
+/**
+ * 作者：flygo
+ * 版本：1.0
+ * 创建日期：2019/5/6
+ * 描述：
+ * 修订历史：
+ */
+public class httpUtil{
+    public static void doGet(String url){
+
+    }
+}
